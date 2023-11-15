e5f8607f753412fc42c8ee9085099e94e8db05a7
David Pérez <deividpa022@gmail.com>
2023-11-15 09:44:11 -0500

    Editorial Entity and its data created

---
+++ src/main/java/com/dpa/LibraryProject/entities/Editorial.java
@@ -0,0 +1,38 @@
+package com.dpa.LibraryProject.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ *
+ * @author David Perez
+ */
+@Entity
+public class Editorial {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name  = "uuid", strategy = "uuid2")
+    private String id;
+    private String name;
+
+    public Editorial() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
