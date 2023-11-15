967cd1e30bc0a73b3086f7b7e53e74d665f5e16c
David Pérez <deividpa022@gmail.com>
2023-11-15 17:40:14 -0500

    Class MyException created in order to differenciate the system errors from the errors produced by the code

---
+++ src/main/java/com/dpa/LibraryProject/exceptions/MyException.java
@@ -0,0 +1,11 @@
+package com.dpa.LibraryProject.exceptions;
+
+/**
+ *
+ * @author David Perez
+ */
+public class MyException extends Exception {
+    MyException(String msg) {
+        super(msg);
+    }
+}
