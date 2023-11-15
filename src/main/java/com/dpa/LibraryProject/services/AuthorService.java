1828fee0117e63de47877c74aa9b8a1bf157ab24
David Pérez <deividpa022@gmail.com>
2023-11-15 12:06:26 -0500

    Services Folder Created. Book, Editorial and Author were created

---
+++ src/main/java/com/dpa/LibraryProject/services/AuthorService.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dpa.LibraryProject.services;
+
+import com.dpa.LibraryProject.entities.Book;
+import java.util.Date;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author David Perez
+ */
+
+@Service
+public class AuthorService {
+    
+}
---
+++ src/main/java/com/dpa/LibraryProject/services/EditorialService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dpa.LibraryProject.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author David Perez
+ */
+@Service
+public class EditorialService {
+    
+}
